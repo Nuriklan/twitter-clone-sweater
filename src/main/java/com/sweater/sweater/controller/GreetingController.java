3daa1ac580c20b724d625ecc8dc44fbcfba9510f
Nuriklan <grapfelt88@gmail.com>
2023-01-26 21:45:22 +0400

    Message filter

--- src/main/java/com/sweater/sweater/controller/GreetingController.java
+++ src/main/java/com/sweater/sweater/controller/GreetingController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
 import java.util.Map;
 
 @Controller
@@ -41,6 +42,25 @@ public class GreetingController {
 
         messageRepository.save(message);
         Iterable<Message> messages = messageRepository.findAll();
+        model.put("messages", messages);
+
+        return "main";
+    }
+
+    @PostMapping("/filter")
+    public String filter(
+            @RequestParam String filter,
+            Map<String, Object> model
+    ) {
+        Iterable<Message> messages;
+
+        if (filter != null && !filter.isEmpty()) {
+            messages = messageRepository.findByTag(filter);
+        }
+         else {
+             messages = messageRepository.findAll();
+        }
+
         model.put("messages", messages);
 
         return "main";
