4ce7fd7573bbd20da893a6295aa85a24dcca798c
Nuriklan <grapfelt88@gmail.com>
2023-01-26 22:26:40 +0400

    Login page

--- src/main/java/com/sweater/sweater/controller/GreetingController.java
+++ src/main/java/com/sweater/sweater/controller/GreetingController.java
@@ -3,12 +3,12 @@ package com.sweater.sweater.controller;
 import com.sweater.sweater.entity.Message;
 import com.sweater.sweater.repository.MessageRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.List;
 import java.util.Map;
 
 @Controller
@@ -17,14 +17,13 @@ public class GreetingController {
     @Autowired
     private MessageRepository messageRepository;
 
-    @GetMapping("/greeting")
-    public String greeting(@RequestParam(name="name",required=false, defaultValue="World") String name,
-                           Map<String, Object> model) {
-        model.put("name", name);
+    @GetMapping("/")
+    public String greeting() {
+
         return "greeting";
     }
 
-    @GetMapping("/")
+    @GetMapping("/main")
     public String main(Map<String, Object> model) {
         Iterable<Message> messages = messageRepository.findAll();
         model.put("messages", messages);
@@ -32,7 +31,7 @@ public class GreetingController {
         return "main";
     }
 
-    @PostMapping("/")
+    @PostMapping("/main")
     public String add(
             @RequestParam String text,
             @RequestParam String tag,
