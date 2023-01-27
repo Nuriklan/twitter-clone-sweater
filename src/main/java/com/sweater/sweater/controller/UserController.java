41470f4cb39861d9ad624495fe334a42f40a925b
Nuriklan <grapfelt88@gmail.com>
2023-01-27 18:36:33 +0400

    User list page

---
+++ src/main/java/com/sweater/sweater/controller/UserController.java
@@ -0,0 +1,24 @@
+package com.sweater.sweater.controller;
+
+import com.sweater.sweater.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @GetMapping
+    public String userList(Model model) {
+
+        model.addAttribute("users", userRepository.findAll());
+
+        return "userList";
+    }
+}
